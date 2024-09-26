e3dd384aada816fda047f6c84b4d098f3325f8b0
SeSMP <se.santamaria@duocuc.cl>
2024-09-26 00:04:09 -0300

    Pagina de inicio de sesion finalizada (Si que si)

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  //Formgroup de login
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { 
+    this.loginForm = this.formBuilder.group({
+      email:['',[Validators.required, Validators.email,this.dominioValidator]],
+      password: ['',[Validators.required]]
+    });
+  }
+
+  //Validador de dominio duocuc
+  dominioValidator(control:any){
+    const email = control.value;
+    if (email && !email.endsWith('@duocuc.cl')){
+      return{domain:true}; //La validación falló
+    }
+    return null; //Validacion correcta
+  }
+
+  onSubmit(){
+    if(this.loginForm.valid){
+      
+    }
+  }
 
   ngOnInit() {
   }
