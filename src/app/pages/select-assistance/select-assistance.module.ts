d557a24e212c8aa25dd0ff90eb4769842d085ad4
SeSMP <se.santamaria@duocuc.cl>
2024-10-08 03:45:50 -0300

    dsa<

--- src/app/pages/select-assistance/select-assistance.module.ts
+++ src/app/pages/select-assistance/select-assistance.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { ComponentsModule } from 'src/app/components/components.module';
 import { IonicModule } from '@ionic/angular';
 
 import { SelectAssistancePageRoutingModule } from './select-assistance-routing.module';
@@ -11,6 +11,7 @@ import { SelectAssistancePage } from './select-assistance.page';
 @NgModule({
   imports: [
     CommonModule,
+    ComponentsModule,
     FormsModule,
     IonicModule,
     SelectAssistancePageRoutingModule
