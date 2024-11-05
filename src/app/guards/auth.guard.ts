f4bbbac4f1f1f1ef519b4b41645aaad1839f6a0f
CETECOM <se.santamaria@duocuc.cl>
2024-11-05 17:08:27 -0300

    creando guard sin servicio auth + servicio auth

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,8 @@
 import { CanActivateFn } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  return new Promise((resolve => {
+    resolve(true);
+    resolve(false);
+  }))
 };
