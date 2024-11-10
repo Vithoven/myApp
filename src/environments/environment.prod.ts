b0c15600fcb0d08eb0fdd562a554eb6c72136bb0
SeSMP <se.santamaria@duocuc.cl>
2024-11-10 20:26:02 -0300

    login terminado ahora si que si

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  
+  firebaseConfig: {
+    apiKey: "AIzaSyB9Q1t6gUGaOBGkkP1M7U1n1zbGEJ4lO2U",
+    authDomain: "registrapp-f83a9.firebaseapp.com",
+    projectId: "registrapp-f83a9",
+    storageBucket: "registrapp-f83a9.appspot.com",
+    messagingSenderId: "1072150469678",
+    appId: "1:1072150469678:web:cf354eb2a62d83f3cb93bf"
+  }
 };
