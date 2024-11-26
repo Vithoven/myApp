43d07e64d9c2f25d8f91413643de3dc529ae26dc
vito <vi.bozzano@duocuc.cl>
2024-11-26 20:41:12 -0300

    otro intento de arreglo para ruta de configuracion

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,8 +1,20 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return new Promise((resolve => {
-    resolve(true);
-    resolve(false);
-  }))
-};
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(): boolean {
+    const isProfesor = JSON.parse(localStorage.getItem('isProfesor') || 'false');
+
+    if (isProfesor !== null) {
+      return true;
+    }
+
+    this.router.navigate(['/login']);
+    return false;
+  }
+}
