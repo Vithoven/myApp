8133f2701f90ef6dc30dbaa8e07ce2acfc9bfcf3
vito <vi.bozzano@duocuc.cl>
2025-03-22 15:26:44 -0300

    inclusion validacion correos

--- src/app/pages/crear/crear.page.ts
+++ src/app/pages/crear/crear.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { UtilsService } from 'src/app/services/utils.service';
 import { FirebaseService } from 'src/app/services/firebase.service';
 
@@ -9,64 +9,71 @@ import { FirebaseService } from 'src/app/services/firebase.service';
   styleUrls: ['./crear.page.scss'],
 })
 export class CrearPage implements OnInit {
-
   
   // Dependencias
   authSvc = inject(FirebaseService);
   utils = inject(UtilsService);
 
+  // Validador personalizado para correos
+  static emailDomainValidator(control: AbstractControl): ValidationErrors | null {
+    const email = control.value;
+    if (email && !email.endsWith('@cosl.cl') && !email.endsWith('@alumnoscosl.cl')) {
+      return { invalidDomain: true };
+    }
+    return null;
+  }
+
   // Formulario
   createUserForm = new FormGroup({
     uname: new FormControl('', [Validators.required, Validators.minLength(4)]),
     ulaname: new FormControl('', [Validators.required, Validators.minLength(4)]),
-    uemail: new FormControl('', [Validators.email, Validators.required]),
+    uemail: new FormControl('', [Validators.required, Validators.email, CrearPage.emailDomainValidator]),
     upassword: new FormControl('', [Validators.required, Validators.minLength(6)]),
     password2: new FormControl('', [Validators.required, Validators.minLength(6)]),
   });
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  // Validacion de contraseña
+  // Validación de contraseña
   validarContraseniasIguales() {
     const pass1 = this.createUserForm.value.upassword;
     const pass2 = this.createUserForm.value.password2;
 
     if (pass1 !== pass2) {
-      this.createUserForm.controls.password2.setErrors({ noIguales: true});
+      this.createUserForm.controls.password2.setErrors({ noIguales: true });
     }
   }
 
   // Registro
-async signUp() {
-  if (this.createUserForm.valid) {
-    const loading = await this.utils.presentLoading();
-    loading.present();
-    const email = this.createUserForm.value.uemail;
-    const password = this.createUserForm.value.upassword;
-    const nombre = this.createUserForm.value.uname;
-    const apellidos = this.createUserForm.value.ulaname;
-    try {
-      const user = await this.authSvc.signUp(email!, password!, nombre!, apellidos!);
-      if (user) {
-        this.utils.navigateForwardto("/login");
+  async signUp() {
+    if (this.createUserForm.valid) {
+      const loading = await this.utils.presentLoading();
+      loading.present();
+      const email = this.createUserForm.value.uemail;
+      const password = this.createUserForm.value.upassword;
+      const nombre = this.createUserForm.value.uname;
+      const apellidos = this.createUserForm.value.ulaname;
+      try {
+        const user = await this.authSvc.signUp(email!, password!, nombre!, apellidos!);
+        if (user) {
+          this.utils.navigateForwardto("/login");
+        }
+      } catch (error) {
+        let errorMessage = 'Error al registrarse';
+        if (!navigator.onLine) {
+          errorMessage = 'No hay conexión a Internet. Por favor, inténtelo de nuevo más tarde.';
+        } else if (error.code === 'auth/email-already-in-use') {
+          errorMessage = 'El correo electrónico ya está registrado. Por favor, use otro correo.';
+        }
+        this.utils.presentToast({
+          icon: 'close-circle-sharp',
+          message: errorMessage,
+          color: 'danger',
+          duration: 2500
+        });
+      } finally {
+        loading.dismiss();
       }
-    } catch (error) {
-      let errorMessage = 'Error al registrarse';
-      if (!navigator.onLine) {
-        errorMessage = 'No hay conexión a Internet. Por favor, inténtelo de nuevo más tarde.';
-      } else if (error.code === 'auth/email-already-in-use') {
-        errorMessage = 'El correo electrónico ya está registrado. Por favor, use otro correo.';
-      }
-      this.utils.presentToast({
-        icon: 'close-circle-sharp',
-        message: errorMessage,
-        color: 'danger',
-        duration: 2500
-      });
-    } finally {
-      loading.dismiss();
     }
   }
-}
 }
